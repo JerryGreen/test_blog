06d6f721920befd2bae4b53bce9d80b11599b459
Alexander Epifanov <jerrygreenest@gmail.com>
2015-07-13 16:00:41 +0500

    Some devise settings added. Still in progress.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,24 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new user_params
+
+    if @project.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params[:project].permit(:name, :description, :user_id)
+    end
 end
